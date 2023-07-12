40d78ed5804f12de018d7c774cc8273e0e920927
jueves <jueves@users.noreply.github.com>
2023-07-12 11:44:35 +0000

    Code cleaning.

--- main.py
+++ main.py
@@ -7,23 +7,24 @@ from io import StringIO
 import openai
 from sanity_check import sanity_check
 
-data_file_name = "data.csv"
+DATA_FILENAME = "data.csv"
+NEW_DATA_FILENAME = "tmp.csv"
 
 # Load keys
 with open("keys.json", "r") as f:
     keys_dic = json.load(f)
 
 # Load text messages
-with open("prompt_header.txt", "r") as f:
+with open("prompt_header.txt", "r", encoding="utf-8") as f:
     prompt_header = f.read()
 
-with open("data_structure.json", "r") as f:
+with open("data_structure.json", "r", encoding="utf-8") as f:
     data_structure = json.load(f)
 
-with open("start.txt", "r") as f:
+with open("start.txt", "r", encoding="utf-8") as f:
     start_message = f.read()
 
-with open("help.txt", "r") as f:
+with open("help.txt", "r", encoding="utf-8") as f:
     help_message = f.read()
 
 
@@ -33,6 +34,9 @@ messages = [ {"role": "system", "content": "You are a intelligent assistant."} ]
 
 # chatGPT functions
 def just_chat(text):
+    '''
+    Takes a string with a message to chatGPT and returns the answer.
+    '''
     messages.append({"role": "user", "content": text})
     chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
     
@@ -40,6 +44,11 @@ def just_chat(text):
     return(reply)
 
 def message_to_csv(message):
+    '''
+    Takes a telebot message object whose text describes a table and converts
+    it to csv using chatGPT.
+    The prompt sent to GPT includes a fixed header describing the table structure.
+    '''
     message_time = datetime.utcfromtimestamp(message.date)
     timestr = message_time.strftime("%d.%m.%Y %H:%M:%S  ")
     txt_input = prompt_header + timestr + message.text
@@ -51,6 +60,10 @@ def message_to_csv(message):
     return(reply)
 
 def gen_markup(add_buttons=False):
+    '''
+    If add_buttons=True, it returns a telebot markup object that adds buttons
+    to the message. Otherwise, it returns an empty markup.
+    '''
     markup = InlineKeyboardMarkup()
     if add_buttons:
         markup.row_width = 2
@@ -58,13 +71,18 @@ def gen_markup(add_buttons=False):
                                 InlineKeyboardButton("Hay errores", callback_data="cb_errors"))
     return markup
 
-def update_dataset(data_file_name=data_file_name):
-    data = pd.read_csv(data_file_name)
-    new_data = pd.read_csv("tmp.csv")
+def update_dataset(data_filename=DATA_FILENAME, new_data_filename=NEW_DATA_FILENAME):
+    '''
+    Takes the names of the whole dataset file and the new data file.
+    Performs an update attaching all new data to the whole dataset.
+    Writes changes to disk.
+    '''
+    data = pd.read_csv(data_filename)
+    new_data = pd.read_csv(new_data_filename)
     print("len(data) en update_dataset() " + str(len(data)))
     print("len(new_data) en update_dataset() " + str(len(new_data)))
     data = pd.concat([data, new_data], ignore_index=True) # FIX THIS: Needs to be merged on left columns.
-    data.to_csv(data_file_name)
+    data.to_csv(data_filename)
 
 def get_table_answer(message):
     add_buttons = True
@@ -87,7 +105,7 @@ bot = telebot.TeleBot(keys_dic["telegram"])
 def callback_query(call):
     if call.data == "cb_correct":
         bot.answer_callback_query(call.id, "Has confirmado la tabla propuesta.")
-        update_dataset(data_file_name)
+        update_dataset(DATA_FILENAME)
     elif call.data == "cb_errors":
         bot.answer_callback_query(call.id, "Has marcado la tabla propuesta como erronea.")
 
