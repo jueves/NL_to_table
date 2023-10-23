73d0b024bbf912869125f7f80d71e26352f03161
jueves <martes1+varios@fastmail.fm>
2023-10-23 10:55:41 +0100

    Compress backup

--- backup_mongo.sh
+++ backup_mongo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DATE="$(date +%Y-%m-%d)"
 BACKUPS_PATH="./db-backups"
-docker run --rm -v nl_to_table_db-data:/db-data -v "$BACKUPS_PATH":/backup ubuntu tar cvf /backup/backup"$DATE".tar /db-data
+docker run --rm -v nl_to_table_db-data:/db-data -v "$BACKUPS_PATH":/backup ubuntu tar -czvf /backup/backup"$DATE".tar /db-data
