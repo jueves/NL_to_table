4ae2f627ef27739c3a0ec881ee8137be6072ea37
jueves <martes1+varios@fastmail.fm>
2023-10-23 10:37:05 +0100

    Set backup path as constant

--- backup_mongo.sh
+++ backup_mongo.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 DATE="$(date +%Y-%m-%d)"
-docker run --rm -v nl_to_table_db-data:/db-data -v ./db-backups:/backup ubuntu tar cvf /backup/backup"$DATE".tar /db-data
+BACKUPS_PATH="./db-backups"
+docker run --rm -v nl_to_table_db-data:/db-data -v "$BACKUPS_PATH":/backup ubuntu tar cvf /backup/backup"$DATE".tar /db-data
